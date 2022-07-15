d061842be006d6f9f208151ca59cbe99450e282d
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-15 12:21:31 +0300

    path added
    After defining the path youtube had been successfully accessed, but with DeprecationWarning: executable_path has been deprecated selenium python

--- youtube_player.py
+++ youtube_player.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(options=chrome_options)
 
 web = 'https://www.youtube.com/'
+path = '/Users/UltraBook/PycharmProjects/pythonProject1/venv/Lib/site-packages/selenium/webdriver/chromium/chromedriver'
+
+driver = webdriver.Chrome(path, options=chrome_options)
 driver.get(web)
