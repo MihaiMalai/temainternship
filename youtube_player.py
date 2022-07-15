161c9ac5195dc1bd21ea38a721f11b809de6bab4
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-15 12:49:00 +0300

    overcome warning  + delete cookies
    found a solution for warning here: https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python

--- youtube_player.py
+++ youtube_player.py
@@ -1,9 +1,21 @@
+import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 chrome_options = webdriver.ChromeOptions()
 
 web = 'https://www.youtube.com/'
-path = '/Users/UltraBook/PycharmProjects/pythonProject1/venv/Lib/site-packages/selenium/webdriver/chromium/chromedriver'
+service = Service('/Users/UltraBook/PycharmProjects/pythonProject1/venv/Lib/site-packages/selenium/webdriver/chromium'
+                  '/chromedriver')
 
-driver = webdriver.Chrome(path, options=chrome_options)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get(web)
+
+# try to reject cookies only in case it is required
+try:
+    reject_cookies = driver.find_element(by=By.XPATH, value='.//tp-yt-paper-button [contains(@class, "style-scope '
+                                                            'ytd-button-renderer style-primary size-default")]')
+    reject_cookies.click()
+except selenium.common.exceptions.NoSuchElementException:
+    pass
