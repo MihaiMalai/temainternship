63a2619adeaf717b3c4a1308c5842ce6f4cc29f2
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-15 12:56:51 +0300

    access the first video

--- youtube_player.py
+++ youtube_player.py
@@ -19,3 +19,12 @@ try:
     reject_cookies.click()
 except selenium.common.exceptions.NoSuchElementException:
     pass
+
+# access the first video in the list using the link from 'href' by full XPath
+first_video = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two'
+                                                     '-column-browse-results-renderer/div['
+                                                     '1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row['
+                                                     '1]/div/ytd-rich-item-renderer[1]/div/ytd-rich-grid-media/div['
+                                                     '1]/ytd-thumbnail/a')
+first_video.get_attribute('href')
+first_video.click()
