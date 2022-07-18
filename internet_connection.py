a69aefcf94930c703f5493c433694ee1194aa5a0
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-18 17:47:01 +0300

    checking internet connection function added

---
+++ internet_connection.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def check_internet():
+    url = "https://www.youtube.com/"
+    timeout = 5
+    try:
+        requests.get(url, timeout=timeout)
+    except (requests.ConnectionError, requests.Timeout):
+        connected = input('No internet access, please try to reconnect. If connected press "y", if failed to '
+                          'connect press "n" to quit: ')
+        if str.lower(connected) != 'y':
+            print('Failed to connect to the internet, process finished')
+            quit()
+        while str.lower(connected) == 'y':
+            try:
+                requests.get(url, timeout=timeout)
+                break
+            except (requests.ConnectionError, requests.Timeout):
+                connected = input('No internet access, please try to reconnect. If connected press "y", if failed to '
+                                  'connect press "n" to quit: ')
+                if connected != 'y':
+                    print('Failed to connect to the internet, process finished')
+                    quit()
