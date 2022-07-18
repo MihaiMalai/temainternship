fe0ca036ad3e3989b5a2d8203657d6ec74573d8b
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-18 18:09:47 +0300

    check internet connection while browsing on youtube
    + fullscreen option added

--- youtube_player.py
+++ youtube_player.py
@@ -3,23 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
+from internet_connection import check_internet
 
 chrome_options = webdriver.ChromeOptions()
 
 web = 'https://www.youtube.com/'
+# provide your file's path to chromedriver
 service = Service('/Users/UltraBook/PycharmProjects/pythonProject1/venv/Lib/site-packages/selenium/webdriver/chromium'
                   '/chromedriver')
+
+check_internet()
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get(web)
 
-# try to reject cookies only in case it is required
+check_internet()
+
+# reject cookies in case it is required
 try:
     reject_cookies = driver.find_element(by=By.XPATH, value='.//tp-yt-paper-button [contains(@class, "style-scope '
                                                             'ytd-button-renderer style-primary size-default")]')
     reject_cookies.click()
 except selenium.common.exceptions.NoSuchElementException:
     pass
+
+check_internet()
 
 # access the first video in the list using the link from 'href' by full XPath
 first_video = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two'
@@ -30,13 +38,21 @@ first_video = driver.find_element(by=By.XPATH, value='/html/body/ytd-app/div[1]/
 first_video.get_attribute('href')
 first_video.click()
 
-# waiting 10 secs to be able to skip the add
-time.sleep(10)
+# waiting to load the video
+time.sleep(3)
+fullscreen = driver.find_element(by=By.XPATH, value='.//button [contains(@class, "ytp-fullscreen-button ytp-button")]')
+fullscreen.click()
+
+# waiting 4 secs to be able to skip the add
+time.sleep(4)
+
+check_internet()
+
 try:
     skip_add = driver.find_element(by=By.XPATH, value='.//button [contains(@class, "ytp-ad-skip-button ytp-button")]')
     skip_add.click()
 except selenium.common.exceptions.NoSuchElementException:
-    time.sleep(25)  # exception in case the add is longer
+    time.sleep(7)  # exception in case the add is longer
     try:
         skip_add = driver.find_element(by=By.XPATH, value='.//button [contains(@class, "ytp-ad-skip-button '
                                                           'ytp-button")]')
