f7850f21c3e350372a2c75839f368475b287a7e7
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-19 21:47:42 +0300

    Create video recording function
    The function captures screenshots one at a time using pyautogui, converts that screenshot to a numpy array which is appended to the frames list. After recording is done, the function loops through frames list to write those numpy arrays to a file with the proper format using a video writer in OpenCV.

---
+++ video_recording.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import pyautogui
+import logging
+
+
+def record_video():
+    # set pause to 0 to make video recording faster
+    pyautogui.PAUSE = 0
+    screen_size = tuple(pyautogui.size())
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    frames_per_second = 10
+
+    out = cv2.VideoWriter("output.avi", fourcc, frames_per_second, screen_size)
+
+    record_seconds = 10
+
+    logging.info('video recording')
+
+    frames = []
+
+    for i in range(int(record_seconds * frames_per_second)):
+        img = pyautogui.screenshot()
+        frames.append(np.array(img))
+
+    logging.info("done video recording")
+    logging.info('processing video')
+
+    for frame in frames:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+
+    logging.info('done processing video')
+
+    out.release()
