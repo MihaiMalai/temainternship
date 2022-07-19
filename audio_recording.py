0b6f77df06d6c8aa834c2e0b2f0bdf556a70caed
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-19 16:43:45 +0300

    record audio
    use for loop to record and store the signals chunk by chunk into the "frames" list for further processing

--- audio_recording.py
+++ audio_recording.py
@@ -1,11 +1,33 @@
 import pyaudio
 import wave
-
+import logging
 
 def record_audio():
     chunk = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    RECORD_SECONDS = 10
-    WAVE_OUTPUT_FILENAME = "output.wav"
+    sample_format = pyaudio.paInt16
+    channels = 1
+    rate = 44100
+    record_seconds = 10
+    wave_output_filename = "output.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=chunk)
+
+    logging.info("audio recording")
+
+    frames = []
+
+    for i in range(int(rate / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    logging.info("done audio recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
