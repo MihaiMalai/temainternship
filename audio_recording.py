05c6c6f2d3a55d623af04dc26c7dd4e06d3a0fdd
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-19 16:06:24 +0300

    define needed variables
    chunk - the number of frames the signals will be split into
    format - assigned size of each frame being 2 bytes
    channels - number of samples per frame
    rate - number of frames per second

--- audio_recording.py
+++ audio_recording.py
@@ -3,4 +3,9 @@ import wave
 
 
 def record_audio():
-    pass
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    RECORD_SECONDS = 10
+    WAVE_OUTPUT_FILENAME = "output.wav"
