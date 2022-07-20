2d6bbd28bf4dc03eef7826314586044d89552d80
MihaiMalai <103949088+MihaiMalai@users.noreply.github.com>
2022-07-20 17:28:52 +0300

    Update analyze_wav.py
    Exception in case the sound file was not created

--- analyze_wav.py
+++ analyze_wav.py
@@ -5,10 +5,13 @@ import logging
 
 def measure_wav_db_level(wav_file):
     logging.info(f'Reading {wav_file} wave audio file')
-    fs, x = wavfile.read(wav_file)
-    log_scale = 20*np.log10(32767)
+    try:
+        fs, x = wavfile.read(wav_file)
+        log_scale = 20*np.log10(32767)
+        # convert data to float64, because it's hard to work on int16
+        t = (np.array(x)).astype(np.float64)
+        orig_SPL = 20 * np.log10(np.sqrt(np.mean(np.absolute(t) ** 2))) - log_scale
+        logging.info(f'Sound level:   {str(orig_SPL)} dBFS')
+    except FileNotFoundError:
+        logging.exception('Exception occurred, unable to provide the sound level')
 
-    # convert data to float64, because it's hard to work on int16
-    t = (np.array(x)).astype(np.float64)
-    orig_SPL = 20*np.log10(np.sqrt(np.mean(np.absolute(t)**2))) - log_scale
-    logging.info(f'Sound level:   {str(orig_SPL)} dBFS')
